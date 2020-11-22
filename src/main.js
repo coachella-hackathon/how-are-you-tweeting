0f2c06401509d2fe0ade2322822d157a788a1a71
claier <claire.xw205@gmail.com>
2020-11-22 09:16:55 +0800

    minor updates

--- src/main.js
+++ src/main.js
@@ -1,7 +1,12 @@
 /* eslint-disable */
 // import "bootstrap";
 import "./style.css";
-import anime from "animejs/lib/anime.es.js";
+var _ = require("lodash");
+const config = require("../config");
+var Chart = require("chart.js");
+
+import { firebase } from "@firebase/app";
+require("firebase/firestore");
 
 // import "./assets/img/rigo-baby.jpg";
 // import "./assets/img/4geeks.ico";
@@ -21,21 +26,84 @@ function getUrlParameter(sParam) {
     }
   }
 }
-var timeout;
-function changeText(username) {
+function changeText(data) {
   var section = document.getElementById("section");
   var title1 = document.getElementById("title-1");
   var title2 = document.getElementById("title-2");
   var content = document.getElementById("content");
 
-  clearTimeout(timeout);
-  timeout = setTimeout(function() {
-    title1.innerHTML = "Hello";
-    title2.innerHTML = "@" + username;
-  }, 8000);
+  title1.innerHTML = data.title1;
+  title2.innerHTML = data.title2;
+  content.innerHTML = data.content;
 }
 window.onload = function() {
-  const username = getUrlParameter("username");
+  // const username = getUrlParameter("username");
+  firebase.initializeApp(config.firebaseConfig);
+  const username = "WanderingQi";
+  var clickCount = 0;
+  var data = [{ title1: "Hello", title2: "@" + username, content: null }];
 
-  changeText(username);
+  var db = firebase.firestore();
+  var docRef = db.collection("users").doc(username);
+
+  // TO remove in prod
+  // var getOptions = {
+  //   source: "cache"
+  // };
+
+  docRef
+    .get()
+    .then(function(doc) {
+      return doc.data();
+    })
+    .then(function(d) {
+      delete d["additionalInfo"];
+      delete d["userCategory"];
+      const arr = Object.values(d);
+
+      const tweetCount = arr.length;
+      data.push({
+        title1: "You Have Tweeted",
+        title2: tweetCount + " Times",
+        content: null
+      });
+
+      var tweetByMonthChartObj = _.countBy(arr, function(obj) {
+        console.log(obj);
+        return new Date(obj.created_at).toLocaleString("default", {
+          month: "short"
+        });
+      });
+      var tweetByMonthChartData = Object.keys(tweetByMonthChartObj).map(k => {
+        return { x: tweetByMonthChartObj[k], y: k };
+      });
+      console.log(tweetByMonthChartData);
+      var tweetByMonthChart = new Chart(
+        document.getElementById("tweetByMonthChart").getContext("2d"),
+        {
+          type: "line",
+          data: tweetByMonthChartData
+          // options: options
+        }
+      );
+      data.push({
+        title1: "And You are So Active on Twitter",
+        title2: null,
+        content: tweetByMonthChart
+      });
+
+      document.getElementById("content").innerHTML = "Click to Continue";
+      console.log(data);
+    })
+    .catch(function(error) {
+      console.log("Error getting cached document:", error);
+    });
+
+  document.getElementById("body").addEventListener("click", function() {
+    console.log(data);
+    if (clickCount < data.length) {
+      changeText(data[clickCount]);
+      clickCount++;
+    }
+  });
 };
